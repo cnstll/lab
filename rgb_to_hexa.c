474c80cec9d210637e8eb73bfac80b84eb703d22
calle <calle@student.42.fr>
2021-03-09 11:32:03 +0100

    experiments on rgb hexa conversion

--- rgb_to_hexa.c
+++ rgb_to_hexa.c
@@ -5,6 +5,17 @@ unsigned long createRGB(int r, int g, int b)
     return (((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff));
 }
 
+int deconstruct_hexa(int color, char rgb)
+{
+	if (rgb == 'R')
+		return ((color & 0xff0000) >> 16);
+	if (rgb == 'G')
+		return ((color & 0x00ff00) >> 8);
+	if (rgb == 'B')
+		return (color & 0x0000ff);
+	return (-1);
+}
+
 unsigned long bitwiseAND(int r)
 {   
     return ((r & 0xff));
@@ -19,5 +30,11 @@ int main()
 	printf("Hex -- %lx\n", bitwiseAND(13));
 	printf("Hex -- %lx\n", bitwiseAND(255));
 	printf("0xff -- %d\n", 0xff);
+	printf("R -- %d\n", deconstruct_hexa(0xff0ff0, 'R'));
+	printf("G -- %d\n", deconstruct_hexa(0xff0ff0, 'G'));
+	printf("B -- %d\n", deconstruct_hexa(0xff0ff0, 'B'));
+	printf("R -- %d\n", deconstruct_hexa(0xc00f80, 'R'));
+	printf("G -- %d\n", deconstruct_hexa(0xc00f80, 'G'));
+	printf("B -- %d\n", deconstruct_hexa(0xc00f80, 'B'));
 	return (0);
 }
